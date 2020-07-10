bce578701a267bf631c235714e6974ccb914aa59
jack1-2-3 <jack.programming123@gmail.com>
2020-07-10 10:17:21 +0900

    pullできなかった部分を修正

---
+++ top/scripts/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const swiperContainer = document.querySelector('.swiper-container');
+    const hero = new HeroSlider('.swiper-container');
+    const cb = (entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                hero.start();
+            } else {
+                hero.stop();
+            };
+        });
+    }
+    const io = new IntersectionObserver(cb);
+    io.observe(swiperContainer);
+})
